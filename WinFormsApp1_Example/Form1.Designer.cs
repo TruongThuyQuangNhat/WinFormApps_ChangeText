6a82d6d0e33c26e2856666e84636f59f6258046c
Nhat <truongthuyquangnhat1@gmail.com>
2022-01-20 10:24:03 +0700

    app change style text

---
+++ WinFormsApp1_Example/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WinFormsApp1_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dauvao = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radGreen = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radYellow = new System.Windows.Forms.RadioButton();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkUnderline = new System.Windows.Forms.CheckBox();
+            this.chkItalic = new System.Windows.Forms.CheckBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.daura = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập tại đây";
+            // 
+            // dauvao
+            // 
+            this.dauvao.Location = new System.Drawing.Point(108, 23);
+            this.dauvao.Name = "dauvao";
+            this.dauvao.Size = new System.Drawing.Size(278, 23);
+            this.dauvao.TabIndex = 1;
+            this.dauvao.TextChanged += new System.EventHandler(this.dauvao_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radGreen);
+            this.groupBox1.Controls.Add(this.radBlue);
+            this.groupBox1.Controls.Add(this.radYellow);
+            this.groupBox1.Controls.Add(this.radRed);
+            this.groupBox1.Location = new System.Drawing.Point(19, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 134);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Màu sắc";
+            // 
+            // radGreen
+            // 
+            this.radGreen.AutoSize = true;
+            this.radGreen.ForeColor = System.Drawing.Color.Lime;
+            this.radGreen.Location = new System.Drawing.Point(6, 105);
+            this.radGreen.Name = "radGreen";
+            this.radGreen.Size = new System.Drawing.Size(63, 19);
+            this.radGreen.TabIndex = 3;
+            this.radGreen.TabStop = true;
+            this.radGreen.Text = "xanh lá";
+            this.radGreen.UseVisualStyleBackColor = true;
+            this.radGreen.CheckedChanged += new System.EventHandler(this.radGreen_CheckedChanged);
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.ForeColor = System.Drawing.Color.Blue;
+            this.radBlue.Location = new System.Drawing.Point(6, 76);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(89, 19);
+            this.radBlue.TabIndex = 2;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "xanh dương";
+            this.radBlue.UseVisualStyleBackColor = true;
+            this.radBlue.CheckedChanged += new System.EventHandler(this.radBlue_CheckedChanged);
+            // 
+            // radYellow
+            // 
+            this.radYellow.AutoSize = true;
+            this.radYellow.ForeColor = System.Drawing.Color.Yellow;
+            this.radYellow.Location = new System.Drawing.Point(6, 47);
+            this.radYellow.Name = "radYellow";
+            this.radYellow.Size = new System.Drawing.Size(51, 19);
+            this.radYellow.TabIndex = 1;
+            this.radYellow.TabStop = true;
+            this.radYellow.Text = "vàng";
+            this.radYellow.UseVisualStyleBackColor = true;
+            this.radYellow.CheckedChanged += new System.EventHandler(this.radYellow_CheckedChanged);
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.ForeColor = System.Drawing.Color.Red;
+            this.radRed.Location = new System.Drawing.Point(6, 22);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(40, 19);
+            this.radRed.TabIndex = 0;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "Đỏ";
+            this.radRed.UseVisualStyleBackColor = true;
+            this.radRed.CheckedChanged += new System.EventHandler(this.radRed_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkUnderline);
+            this.groupBox2.Controls.Add(this.chkItalic);
+            this.groupBox2.Controls.Add(this.chkBold);
+            this.groupBox2.Location = new System.Drawing.Point(222, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 106);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Style chữ";
+            // 
+            // chkUnderline
+            // 
+            this.chkUnderline.AutoSize = true;
+            this.chkUnderline.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.chkUnderline.Location = new System.Drawing.Point(6, 76);
+            this.chkUnderline.Name = "chkUnderline";
+            this.chkUnderline.Size = new System.Drawing.Size(79, 19);
+            this.chkUnderline.TabIndex = 2;
+            this.chkUnderline.Text = "gạch dưới";
+            this.chkUnderline.UseVisualStyleBackColor = true;
+            this.chkUnderline.CheckedChanged += new System.EventHandler(this.chkUnderline_CheckedChanged);
+            // 
+            // chkItalic
+            // 
+            this.chkItalic.AutoSize = true;
+            this.chkItalic.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.chkItalic.Location = new System.Drawing.Point(6, 49);
+            this.chkItalic.Name = "chkItalic";
+            this.chkItalic.Size = new System.Drawing.Size(83, 19);
+            this.chkItalic.TabIndex = 1;
+            this.chkItalic.Text = "in nghiêng";
+            this.chkItalic.UseVisualStyleBackColor = true;
+            this.chkItalic.CheckedChanged += new System.EventHandler(this.chkItalic_CheckedChanged);
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkBold.Location = new System.Drawing.Point(6, 22);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(64, 19);
+            this.chkBold.TabIndex = 0;
+            this.chkBold.Text = "in đậm";
+            this.chkBold.UseVisualStyleBackColor = true;
+            this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "kết quả";
+            // 
+            // daura
+            // 
+            this.daura.AutoSize = true;
+            this.daura.Cursor = System.Windows.Forms.Cursors.Default;
+            this.daura.Location = new System.Drawing.Point(80, 211);
+            this.daura.Name = "daura";
+            this.daura.Size = new System.Drawing.Size(0, 15);
+            this.daura.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 250);
+            this.Controls.Add(this.daura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dauvao);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox dauvao;
+        private GroupBox groupBox1;
+        private RadioButton radGreen;
+        private RadioButton radBlue;
+        private RadioButton radYellow;
+        private RadioButton radRed;
+        private GroupBox groupBox2;
+        private CheckBox chkUnderline;
+        private CheckBox chkItalic;
+        private CheckBox chkBold;
+        private Label label2;
+        private Label daura;
+    }
+}
---
+++ WinFormsApp1_Example/Form1.cs
@@ -0,0 +1,55 @@
+﻿namespace WinFormsApp1_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dauvao_TextChanged(object sender, EventArgs e)
+        {
+            daura.Text = dauvao.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radBlue.Checked = true;
+        }
+
+        private void radRed_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.ForeColor = Color.Red;
+        }
+
+        private void radYellow_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.ForeColor = Color.Yellow;
+        }
+
+        private void radBlue_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.ForeColor = Color.Blue;
+        }
+
+        private void radGreen_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.ForeColor = Color.Green;
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.Font = new Font(daura.Font.Name, daura.Font.Size, daura.Font.Style ^ FontStyle.Bold);
+        }
+
+        private void chkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.Font = new Font(daura.Font.Name, daura.Font.Size, daura.Font.Style ^ FontStyle.Italic);
+        }
+
+        private void chkUnderline_CheckedChanged(object sender, EventArgs e)
+        {
+            daura.Font = new Font(daura.Font.Name, daura.Font.Size, daura.Font.Style ^ FontStyle.Underline);
+        }
+    }
+}
